e7b87db4e8d457a8a94fd69deff989b751acc242
devjep <jeffrid.laxamana@gmail.com>
2025-04-02 12:01:47 +0800

    laravel practice 2 day 3

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,24 @@
 <?php
 
-use Illuminate\Support\Arr;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'Director',
-        'salary' => '$50,000',
-    ],
-    [
-        'id' => 2,
-        'title' => 'Programmer',
-        'salary' => '$10,000',
-    ],
-    [
-        'id' => 3,
-        'title' => 'Teacher',
-        'salary' => '$40,000',
-    ]
 
-];
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function ()  use ($jobs){
-    return view('jobs',['jobs'  => $jobs]
+Route::get('/jobs', function ()  {
+    return view('jobs',['jobs'  => Job::all()]
     );
 });
 
 
-Route::get('/jobs/{id}',function($id) use ($jobs){
-    $job = Arr::first($jobs, fn($job)=> $job['id'] == $id);
+Route::get('/jobs/{id}',function($id) {
+    $job = Job::find($id);
     return view('job',['job' => $job ]);
 
 });
