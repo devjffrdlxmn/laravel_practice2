571268b6882f30735431a39b2ef045d95036f81c
devjep <jeffrid.laxamana@gmail.com>
2025-04-15 08:43:09 +0800

    laravelpractice2 day 9 make policies

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,9 +2,9 @@
 
     <h2 class= "font-bold text-lg">{{ $job->title }}</h2>
     <p>This job pay {{ $job->salary }} per year.</p>
-
+    @can('edit',$job)
     <p class= "mt-4">
         <a href="/jobs/{{$job->id}}/edit"  class="rounded-md bg-amber-600 px-3 py-2  text-sm font-semibold text-white shadow-xs hover:bg-amber-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">EDIT JOB</a>
     </p>
-
+    @endcan
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,29 @@ Route::get('/', function () {
 });
 
 
-Route::view('/','home');
-Route::view('/contact','contact');
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::resource('jobs',JobController::class);
 
+// Route::resource('jobs', JobController::class);
+
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit');
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 //Auth
 
-Route::get('/register',[ RegisterUserController::class,'create' ]);
-Route::post('/register',[ RegisterUserController::class,'store'] );
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
 
 
-Route::get('/login',[ SessionController::class,'create']);
-Route::post('/login',[ SessionController::class,'store']);
-Route::post('/logout',[ SessionController::class,'destroy']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
