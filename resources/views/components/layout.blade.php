b0c56eb863048bc880570e36166530cb43f0ae4f
devjep <jeffrid.laxamana@gmail.com>
2025-04-21 09:41:34 +0800

    laravelpractice2 day 10 learning tailwindcss import to app css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite('resources/css/app.css')
+
+
     </head>
 
 <body class="h-full">
